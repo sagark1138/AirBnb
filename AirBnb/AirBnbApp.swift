2251a020545dba1ef131e07994381189eaaf2498
Sagar Kulkarni <sagarkulkarni1138@gmail.com>
2025-02-20 15:38:37 +0530

    Initial Commit

---
+++ AirBnb/AirBnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnbApp.swift
+//  AirBnb
+//
+//  Created by Sagar Kulkarni on 20/02/25.
+//
+
+import SwiftUI
+
+@main
+struct AirBnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
