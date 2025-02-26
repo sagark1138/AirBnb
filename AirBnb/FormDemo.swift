92447c769acf6c4ceaf7133a8c3996646d37f2d6
Sagar Kulkarni <sagarkulkarni1138@gmail.com>
2025-02-26 19:45:51 +0530

    Form components in progress

---
+++ AirBnb/FormDemo.swift
@@ -0,0 +1,18 @@
+//
+//  FormDemo.swift
+//  AirBnb
+//
+//  Created by Sagar Kulkarni on 20/02/25.
+//
+
+import SwiftUI
+
+struct FormDemo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FormDemo()
+}
